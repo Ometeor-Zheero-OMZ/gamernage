29a8677628925ce19279584043d86931f2e129bb
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-08-24 22:47:49 +0900

    TODO用のテーブルを追加

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS restaurant_tables;
 CREATE TABLE restaurant_tables (
-  id INTEGER primary key generated always as identity,
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   table_number INTEGER not null,
   note VARCHAR(500) DEFAULT null,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -21,18 +21,23 @@ INSERT INTO restaurant_tables (table_number, note) VALUES (8, null);
 INSERT INTO restaurant_tables (table_number, note) VALUES (9, null);
 INSERT INTO restaurant_tables (table_number, note) VALUES (10, null);
 
--- Create users table
+-- ユーザー情報
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INTEGER primary key generated always as identity,
-  name VARCHAR(50) NOT NULL,
-  password VARCHAR(61) NOT null,
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(60) NOT NULL,
+  email VARCHAR(250) NULL,
+  profile_image BYTEA NULL,
+  password VARCHAR(61) NOT NULL,
+  verified_email VARCHAR(250) NULL,
+  token TEXT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE(name)
 );
 CREATE UNIQUE INDEX unique_user on users (name);
 
--- add users
+-- テストユーザーの追加
 INSERT INTO users ("name","password") VALUES
 	 ('test_user1','$2b$04$BuM27R11fuD0hubq.Nykd.aw.WDI8F2/lYCPabzfLdGG1GHvYqR/i'),
 	 ('test_user2','$2b$04$BuM27R11fuD0hubq.Nykd.aw.WDI8F2/lYCPabzfLdGG1GHvYqR/i'),
@@ -58,7 +63,7 @@ INSERT INTO users ("name","password") VALUES
 -- Create menus table
 DROP TABLE IF EXISTS menus;
 CREATE TABLE menus (
-  id INTEGER primary key generated always as identity,
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50) NOT NULL,
   cook_time_seconds INTEGER NOT null,
   price integer NOT NULL,
@@ -113,3 +118,41 @@ CREATE TABLE orders (
   deleted_at TIMESTAMP DEFAULT NULL
 );
 CREATE INDEX index_orders on orders (restaurant_table_id, menu_id, served_by_user_id, checked_by_user_id);
+
+-- ゲーム情報
+DROP TABLE IF EXISTS games;
+CREATE TABLE games (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  genre VARCHAR(20) NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE UNIQUE INDEX index_unique_games on games (title);
+CREATE INDEX index_games_genre ON games (genre);
+
+-- TODO
+DROP TABLE IF EXISTS todos;
+CREATE TABLE todos (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER NULL,
+  game_id INTEGER NULL,
+  title VARCHAR(100) NOT NULL,
+  description varchar(255) NOT NULL,
+  is_completed BOOLEAN NOT NULL,
+  status INTEGER NULL,
+  priority INTEGER NULL,
+  difficulty INTEGER NULL,
+  deadline TIMESTAMP NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE SET NULL
+);
+CREATE INDEX index_todo_status ON todos (status);
+CREATE INDEX index_todo_priority ON todos (priority);
+CREATE INDEX index_todo_difficulty ON todos (difficulty);
