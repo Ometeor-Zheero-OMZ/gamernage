891edad243684c657157dfa6bbfcf24abf323892
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-08-24 22:51:24 +0900

    型定義ファイルを追加

---
+++ frontend/constants/type.ts
@@ -0,0 +1,29 @@
+// タスク
+export type TaskParam = {
+  id: number;
+  user_id: number;
+  game_id: number;
+  title: string;
+  description: string;
+  is_completed: boolean;
+  status: number;
+  priority: number;
+  difficulty: number;
+  deadline: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at?: string;
+};
+
+// コラム　タスクとは1対多の関係
+export type ColumnProps = {
+  tasks: TaskParam[];
+  onUpdateTask: (updatedTask: TaskParam) => void;
+  onDeleteTask: (taskId: number) => void;
+  onCompleteTask: (taskId: number) => void;
+};
+
+// 入力フォーム
+export type InputProps = {
+  onSubmit: (title: string, description: string) => void;
+};
