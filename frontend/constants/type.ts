b97c59ac2fbe179e7750ad75d5da58227e86fe49
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-08-26 01:36:29 +0900

    Task.tsxに渡す関数の型定義をいったんコメントアウト

--- frontend/constants/type.ts
+++ frontend/constants/type.ts
@@ -1,26 +1,26 @@
 // タスク
 export type TaskParam = {
   id: number;
-  user_id: number;
-  game_id: number;
+  user_id?: number;
+  game_id?: number;
   title: string;
   description: string;
   is_completed: boolean;
-  status: number;
-  priority: number;
-  difficulty: number;
-  deadline: string;
-  created_at: string;
-  updated_at: string;
+  status?: number;
+  priority?: number;
+  difficulty?: number;
+  deadline?: string;
+  created_at?: string;
+  updated_at?: string;
   deleted_at?: string;
 };
 
 // コラム　タスクとは1対多の関係
 export type ColumnProps = {
   tasks: TaskParam[];
-  onUpdateTask: (updatedTask: TaskParam) => void;
-  onDeleteTask: (taskId: number) => void;
-  onCompleteTask: (taskId: number) => void;
+  // onUpdateTask: (updatedTask: TaskParam) => void;
+  // onDeleteTask: (taskId: number) => void;
+  // onCompleteTask: (taskId: number) => void;
 };
 
 // 入力フォーム
