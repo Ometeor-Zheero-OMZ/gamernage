91e6e44088ec25b83ba27aa6dbc93314b7181d8b
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-08-29 02:15:36 +0900

    認証前のユーザーのリダイレクト先をトップページに変更

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
   useEffect(() => {
     if (!loading && !user) {
-      router.push("/login");
+      router.push("/top");
     }
   }, [user, loading, router]);
 
