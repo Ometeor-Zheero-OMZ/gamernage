3e66c16bb0c6aa970c5cb5b1687edceff1824966
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-09-01 14:03:26 +0900

    メッセージファイルを作成

---
+++ frontend/constants/message.ts
@@ -0,0 +1,21 @@
+export const ERROR_MESSAGES = {
+  LOGIN_FAILED_MESSAGE: "ログインに失敗しました。",
+  LOAD_USER_FAILED_MESSAGE: "ユーザーの読み込みに失敗しました。",
+  INVALID_DATA_FORMAT_NOT_ARRAY_MESSAGE:
+    "データ形式が正しくありません。Array型を期待しています。",
+  FETCH_MISSION_FAILED_MESSAGE: "ミッションの取得に失敗しました。",
+  APPEND_MISSION_FAILED_MESSAGE: "ミッションの追加に失敗しました。",
+  UPDATE_MISSION_FAILED_MESSAGE: "ミッションの更新に失敗しました。",
+  DELETE_MISSION_FAILED_MESSAGE: "ミッションの削除に失敗しました。",
+  PUT_MISSION_STATUS_FAILED_MESSAGE:
+    "ミッションのステータスの変更に失敗しました。",
+};
+
+export const PG_ERROR_MESSAGES = {
+  USEAUTH_REQUIRED_MESSAGE:
+    "AuthProvider 内で useAuth を使用する必要があります。",
+};
+
+export const DYNAMIC_ERROR_MESSAGES = (message: any) => ({
+  FETCH_CURRENT_USER_FAILED_MESSAGE: `現在のユーザーの取得に失敗しました： [${message}]`,
+});
