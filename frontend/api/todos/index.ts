57b1b9283e55cb37f0b09ea8ba60454e605b49e2
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-09-11 02:48:43 +0900

    Divided api calls into api directory

---
+++ frontend/api/todos/index.ts
@@ -0,0 +1,82 @@
+import axios from "axios";
+
+// Fetch tasks
+export const fetchTasks = async (token: string) => {
+  try {
+    const response = await axios.get("/api/todos", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch tasks");
+  }
+};
+
+// Add task
+export const addTask = async (
+  token: string,
+  title: string,
+  description: string
+) => {
+  try {
+    await axios.post(
+      "/api/todo",
+      { title, description },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    throw new Error("Failed to add task");
+  }
+};
+
+// Update task
+export const updateTask = async (token: string, updatedTask: any) => {
+  try {
+    await axios.post("/api/todo", updatedTask, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    throw new Error("Failed to update task");
+  }
+};
+
+// Delete task
+export const deleteTask = async (token: string, taskId: number) => {
+  try {
+    await axios.delete("/api/todo", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: { id: taskId },
+    });
+  } catch (error) {
+    throw new Error("Failed to delete task");
+  }
+};
+
+// Change task status
+export const changeTaskStatus = async (token: string, taskId: number) => {
+  try {
+    await axios.post(
+      "/api/todo/complete",
+      { id: taskId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    throw new Error("Failed to change task status");
+  }
+};
