a4a94ae08196ac2a272a02c3d1f9f65575988ff2
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-12-09 00:33:45 +0900

    feat: DI に関するドキュメントを追加

--- backend/src/application/types/di_type.rs
+++ backend/src/application/types/di_type.rs
@@ -1,11 +1,17 @@
+//! 依存関係の型定義
+//! 
+//! # 例
+//! Arc<dyn ...>
+
 use std::sync::Arc;
 use crate::domain::{repositories::{auth_repository::AuthRepository, todo_repository::TodoRepository, user_repository::UserRepository}, services::{auth_service::AuthService, todo_service::TodoService, user_service::UserService}};
 
+// 認証
 pub type AuthServiceArc = Arc<dyn AuthService>;
 pub type AuthRepositoryArc = Arc<dyn AuthRepository>;
-
+// タスク
 pub type TodoServiceArc = Arc<dyn TodoService>;
 pub type TodoRepositoryArc = Arc<dyn TodoRepository>;
-
+// ユーザー
 pub type UserServiceArc = Arc<dyn UserService>;
 pub type UserRepositoryArc = Arc<dyn UserRepository>;
